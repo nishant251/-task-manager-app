b9e0065b36401b53bc59dd893b97d0f5418415be
Nishant Srivastava <nishantsrivastava864@gmail.com>
2026-03-13 06:21:00 +0530

    fix render port

--- task-manager-backend/server.js
+++ task-manager-backend/server.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
   res.send("Task Manager Backend Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost: ${PORT}`);
