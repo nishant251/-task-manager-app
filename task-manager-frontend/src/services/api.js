e32120dbd1dab3ac1320fec84748f8c6203826bb
Nishant Srivastava <nishantsrivastava864@gmail.com>
2026-03-13 07:42:53 +0530

    url chage

--- task-manager-frontend/src/services/api.js
+++ task-manager-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://task-manager-app-cxn7.onrender.com/api"
+  baseURL: "https://task-manager-app-w0ax.onrender.com"
 });
 
 export default API;
